feca5db79fb6931d527a0882f81593e2015c74fa
reimar <reimar@b3059339-0415-0410-9bf9-f77b7e298cf2>
2010-07-01 19:48:00 +0000

    Do not try to grab input from -wid windows, it will most likely break their input processing. If someone needs the previous behaviour, an option could be added (and also used in the w32_common.c code).
    git-svn-id: svn://git.mplayerhq.hu/mplayer/trunk@31601 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libvo/x11_common.c
+++ libvo/x11_common.c
@@ -1083,6 +1083,10 @@ void vo_x11_create_vo_window(XVisualInfo *vis, int x, int y,
       XInstallColormap(mDisplay, col_map);
     }
     if (WinID) vo_x11_update_geometry();
+    else
+        // Do not capture events since it might break the parent application
+        // if it relies on events being forwarded to the parent of WinID.
+        // It also is consistent with the w32_common.c code.
     vo_x11_selectinput_witherr(mDisplay, vo_window,
           StructureNotifyMask | KeyPressMask | PointerMotionMask |
           ButtonPressMask | ButtonReleaseMask | ExposureMask);
