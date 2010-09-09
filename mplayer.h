572918a5bb15fd9437a33a2685630e8613f8322f
diego <diego@b3059339-0415-0410-9bf9-f77b7e298cf2>
2010-09-09 18:40:30 +0000

    Remove unused video_driver/audio_driver extern variable declarations.
    git-svn-id: svn://git.mplayerhq.hu/mplayer/trunk@32126 b3059339-0415-0410-9bf9-f77b7e298cf2

--- mplayer.h
+++ mplayer.h
@@ -25,8 +25,6 @@ extern char ** audio_fm_list;
 extern char ** video_fm_list;
 extern char ** video_driver_list;
 extern char ** audio_driver_list;
-extern char * video_driver;
-extern char * audio_driver;
 extern float  audio_delay;
 
 extern int osd_level;
