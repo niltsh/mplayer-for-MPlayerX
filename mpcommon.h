dd0c0af2e1ffa9f82269caaaadb54a853c955f3a
reimar <reimar@b3059339-0415-0410-9bf9-f77b7e298cf2>
2010-09-12 11:21:12 +0000

    Remove stheader.h include from mpcommon.h since it conflicts with the windows.h header and thus causes compilation errors e.g. in asf_streaming_mmst.c.
    git-svn-id: svn://git.mplayerhq.hu/mplayer/trunk@32179 b3059339-0415-0410-9bf9-f77b7e298cf2

--- mpcommon.h
+++ mpcommon.h
@@ -23,7 +23,6 @@
 #include "m_option.h"
 #include "subreader.h"
 #include "libmpdemux/demuxer.h"
-#include "libmpdemux/stheader.h"
 #include "libass/ass_mp.h"
 
 extern double sub_last_pts;
@@ -62,8 +61,8 @@ extern m_config_t *mconfig;
 extern const m_option_t noconfig_opts[];
 
 void print_version(const char* name);
-void update_subtitles(sh_video_t *sh_video, double refpts, demux_stream_t *d_dvdsub, int reset);
-void update_teletext(sh_video_t *sh_video, demuxer_t *demuxer, int reset);
+void update_subtitles(struct sh_video *sh_video, double refpts, demux_stream_t *d_dvdsub, int reset);
+void update_teletext(struct sh_video *sh_video, demuxer_t *demuxer, int reset);
 int select_audio(demuxer_t* demuxer, int audio_id, char* audio_lang);
 void set_osd_subtitle(subtitle *subs);
 
