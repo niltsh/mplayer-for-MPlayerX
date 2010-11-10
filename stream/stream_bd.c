2ebe1a6dd792de95bc4381d6c37df214c32639b3
reimar <reimar@b3059339-0415-0410-9bf9-f77b7e298cf2>
2010-11-10 17:43:48 +0000

    Support reading larger blocks in one go for bd:// protocol.
    git-svn-id: svn://git.mplayerhq.hu/mplayer/trunk@32617 b3059339-0415-0410-9bf9-f77b7e298cf2

--- stream/stream_bd.c
+++ stream/stream_bd.c
@@ -276,9 +276,7 @@ static int bd_get_uks(struct bd_priv *bd)
 static off_t bd_read(struct bd_priv *bd, uint8_t *buf, int len)
 {
     int read_len;
-    // TODO: would be nicer if this function also worked for larger reads
-    int max_len = BD_UNIT_SIZE - (bd->pos % BD_UNIT_SIZE);
-    len = FFMIN(len, max_len);
+    int unit_offset = bd->pos % BD_UNIT_SIZE;
     len &= ~15;
     if (!len)
         return 0;
@@ -287,10 +285,14 @@ static off_t bd_read(struct bd_priv *bd, uint8_t *buf, int len)
     if (read_len != len)
         return -1;
 
-    if (bd->pos % BD_UNIT_SIZE) {
-        // decrypt in place
-        av_aes_crypt(bd->aescbc, buf, buf, len / 16, bd->iv.u8, 1);
-    } else {
+    if (unit_offset) {
+        int decrypt_len = BD_UNIT_SIZE - unit_offset;
+        av_aes_crypt(bd->aescbc, buf, buf, decrypt_len / 16, bd->iv.u8, 1);
+        buf += decrypt_len;
+        len -= decrypt_len;
+    }
+    while (len) {
+        int decrypt_len = FFMIN(len, BD_UNIT_SIZE);
         // reset aes context as at start of unit
         key enc_seed;
         bd->iv = BD_CBC_IV;
@@ -307,7 +309,9 @@ static off_t bd_read(struct bd_priv *bd, uint8_t *buf, int len)
 
         // decrypt
         av_aes_crypt(bd->aescbc, &buf[16], &buf[16],
-                     (len - 16) / 16, bd->iv.u8, 1);
+                     (decrypt_len - 16) / 16, bd->iv.u8, 1);
+        buf += decrypt_len;
+        len -= decrypt_len;
     }
 
     bd->pos += read_len;
@@ -461,6 +465,7 @@ static int bd_stream_open(stream_t *s, int mode, void* opts, int* file_format)
         bd->device = DEFAULT_BD_DEVICE;
 
     s->sector_size = BD_UNIT_SIZE;
+    s->read_chunk  = 32 * BD_UNIT_SIZE;
     s->flags       = STREAM_READ | MP_STREAM_SEEK;
     s->fill_buffer = bd_stream_fill_buffer;
     s->seek        = bd_stream_seek;
