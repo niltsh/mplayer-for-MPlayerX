540d8478360b8f3dc4996c47963cbe302c9fc4ef
reimar <reimar@b3059339-0415-0410-9bf9-f77b7e298cf2>
2011-02-27 15:14:23 +0000

    Do not run svn or otherwise try to extract the svn revision when we have a VERSION file we'll be using anyway.
    git-svn-id: svn://git.mplayerhq.hu/mplayer/trunk@32978 b3059339-0415-0410-9bf9-f77b7e298cf2

--- version.sh
+++ version.sh
@@ -2,6 +2,10 @@
 
 test "$1" && extra="-$1"
 
+# releases extract the version number from the VERSION file
+version=$(cat VERSION 2> /dev/null)
+
+if test -z $version ; then
 # Extract revision number from file used by daily tarball snapshots
 # or from the places different Subversion versions have it.
 svn_revision=$(cat snapshot_version 2> /dev/null)
@@ -10,10 +14,8 @@ test $svn_revision || svn_revision=$(grep revision .svn/entries 2>/dev/null | cu
 test $svn_revision || svn_revision=$(sed -n -e '/^dir$/{n;p;q;}' .svn/entries 2>/dev/null)
 test $svn_revision && svn_revision=SVN-r$svn_revision
 test $svn_revision || svn_revision=UNKNOWN
-
-# releases extract the version number from the VERSION file
-version=$(cat VERSION 2> /dev/null)
-test $version || version=$svn_revision
+version=$svn_revision
+fi
 
 NEW_REVISION="#define VERSION \"${version}${extra}\""
 OLD_REVISION=$(head -n 1 version.h 2> /dev/null)
