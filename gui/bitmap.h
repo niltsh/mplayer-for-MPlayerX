95cc60eead1bdec02d7440a3daecaf50bd68f2f4
ib <ib@b3059339-0415-0410-9bf9-f77b7e298cf2>
2011-03-02 17:17:19 +0000

    Cosmetic: Format to MPlayer coding style.
    Additionally: remove needless identifiers, group and sort includes,
    group statements by adding or removing new lines and rearrange statements
    and variables to ease reading.
    
    
    git-svn-id: svn://git.mplayerhq.hu/mplayer/trunk@33000 b3059339-0415-0410-9bf9-f77b7e298cf2

--- gui/bitmap.h
+++ gui/bitmap.h
@@ -19,16 +19,15 @@
 #ifndef MPLAYER_GUI_BITMAP_H
 #define MPLAYER_GUI_BITMAP_H
 
-typedef struct txSample
-{
- unsigned long Width;
- unsigned long Height;
- unsigned int  BPP;
- unsigned long ImageSize;
- char *        Image;
+typedef struct {
+    unsigned long Width;
+    unsigned long Height;
+    unsigned int BPP;
+    unsigned long ImageSize;
+    char *Image;
 } txSample;
 
-int bpRead( char * fname, txSample * bf );
-void Convert32to1( txSample * in,txSample * out,int adaptivlimit );
+int bpRead(char *, txSample *);
+void Convert32to1(txSample *, txSample *, int);
 
 #endif /* MPLAYER_GUI_BITMAP_H */
