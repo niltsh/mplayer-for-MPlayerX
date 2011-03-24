f2cae7631114c519d64d4a9edf446dd4d7b2cfe4
reimar <reimar@b3059339-0415-0410-9bf9-f77b7e298cf2>
2011-03-24 21:56:34 +0000

    Change fifo to not waste one entry needlessly.
    git-svn-id: svn://git.mplayerhq.hu/mplayer/trunk@33101 b3059339-0415-0410-9bf9-f77b7e298cf2

--- mp_fifo.c
+++ mp_fifo.c
@@ -24,12 +24,11 @@
 
 int key_fifo_size = 7;
 static int *key_fifo_data = NULL;
-static int key_fifo_read=0;
-static int key_fifo_write=0;
+static unsigned key_fifo_read=0;
+static unsigned key_fifo_write=0;
 
 static void mplayer_put_key_internal(int code){
-  int fifo_free = key_fifo_read - key_fifo_write - 1;
-  if (fifo_free < 0) fifo_free += key_fifo_size;
+  int fifo_free = key_fifo_read + key_fifo_size - key_fifo_write;
 //  printf("mplayer_put_key(%d)\n",code);
   if (key_fifo_data == NULL)
     key_fifo_data = malloc(key_fifo_size * sizeof(int));
@@ -37,8 +36,8 @@ static void mplayer_put_key_internal(int code){
   // reserve some space for key release events to avoid stuck keys
   if((code & MP_KEY_DOWN) && fifo_free < (key_fifo_size >> 1))
     return;
-  key_fifo_data[key_fifo_write]=code;
-  key_fifo_write=(key_fifo_write+1)%key_fifo_size;
+  key_fifo_data[key_fifo_write % key_fifo_size]=code;
+  key_fifo_write++;
 }
 
 int mplayer_get_key(int fd){
@@ -47,8 +46,8 @@ int mplayer_get_key(int fd){
   if (key_fifo_data == NULL)
     return MP_INPUT_NOTHING;
   if(key_fifo_write==key_fifo_read) return MP_INPUT_NOTHING;
-  key=key_fifo_data[key_fifo_read];
-  key_fifo_read=(key_fifo_read+1)%key_fifo_size;
+  key=key_fifo_data[key_fifo_read % key_fifo_size];
+  key_fifo_read++;
 //  printf("mplayer_get_key => %d\n",key);
   return key;
 }
