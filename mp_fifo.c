0b5d335e3d330b95d8741e4d63e0dd0185c4a2c1
reimar <reimar@b3059339-0415-0410-9bf9-f77b7e298cf2>
2011-03-24 22:28:17 +0000

    Change condition to avoid needless key state resets with very small key fifos.
    git-svn-id: svn://git.mplayerhq.hu/mplayer/trunk@33105 b3059339-0415-0410-9bf9-f77b7e298cf2

--- mp_fifo.c
+++ mp_fifo.c
@@ -33,7 +33,8 @@ static void mplayer_put_key_internal(int code){
     key_fifo_data = malloc(key_fifo_size * sizeof(int));
   if(!fifo_free) return; // FIFO FULL!!
   // reserve some space for key release events to avoid stuck keys
-  if((code & MP_KEY_DOWN) && fifo_free < (key_fifo_size >> 1))
+  // Make sure we do not reset key state because of a down event
+  if((code & MP_KEY_DOWN) && fifo_free <= (key_fifo_size >> 1))
     return;
   // in the worst case, just reset key state
   if (fifo_free == 1)
