926eb8929b658a8459fff3aefe3b408026f09604
ib <ib@b3059339-0415-0410-9bf9-f77b7e298cf2>
2011-04-15 14:30:58 +0000

    Cosmetic: Format to MPlayer coding style.
    Additionally: remove needless includes, group and sort includes, group
    and sort variables, rename gtkAOFakeSurround declaration gtkAOSurround,
    add #ifdefs to variable declarations, group statements by adding or
    removing new lines to ease reading, move assignments outside conditions,
    add parentheses, avoid mixing declaration and code, revise comments and
    add new ones.
    
    
    git-svn-id: svn://git.mplayerhq.hu/mplayer/trunk@33266 b3059339-0415-0410-9bf9-f77b7e298cf2

--- gui/cfg.h
+++ gui/cfg.h
@@ -21,49 +21,54 @@
 
 #include "config.h"
 
+extern int gtkCacheOn;
+extern int gtkCacheSize;
+
+extern int gtkVfLAVC;
+extern int gtkVfPP;
+
+#ifdef CONFIG_DXR3
+extern char *gtkDXR3Device;
+#endif
+
+extern int gtkAutoSyncOn;
+extern int gtkAutoSync;
+
+extern int gtkAONorm;
+extern int gtkAOSurround;
+extern int gtkAOExtraStereo;
+extern float gtkAOExtraStereoMul;
+
+#ifdef CONFIG_ALSA
+extern char *gtkAOALSAMixer;
+extern char *gtkAOALSAMixerChannel;
+extern char *gtkAOALSADevice;
+#endif
+
+#ifdef CONFIG_OSS_AUDIO
+extern char *gtkAOOSSMixer;
+extern char *gtkAOOSSMixerChannel;
+extern char *gtkAOOSSDevice;
+#endif
+
+#ifdef CONFIG_ESD
+extern char *gtkAOESDDevice;
+#endif
+
+#ifdef CONFIG_SDL
+extern char *gtkAOSDLDriver;
+#endif
+
 extern int gtkEnableAudioEqualizer;
+extern char *gtkEquChannel1;
+extern char *gtkEquChannel2;
+extern char *gtkEquChannel3;
+extern char *gtkEquChannel4;
+extern char *gtkEquChannel5;
+extern char *gtkEquChannel6;
 
-extern int    gtkVfPP;
-extern int    gtkVfLAVC;
-
-extern int    gtkAONorm;
-extern int    gtkAOFakeSurround;
-extern int    gtkAOExtraStereo;
-extern float  gtkAOExtraStereoMul;
-extern char * gtkAOOSSMixer;
-extern char * gtkAOOSSMixerChannel;
-extern char * gtkAOOSSDevice;
-extern char * gtkAOALSAMixer;
-extern char * gtkAOALSAMixerChannel;
-extern char * gtkAOALSADevice;
-extern char * gtkAOSDLDriver;
-extern char * gtkAOESDDevice;
-extern char * gtkDXR3Device;
-
-extern int    gtkCacheOn;
-extern int    gtkCacheSize;
-
-extern int    gtkAutoSyncOn;
-extern int    gtkAutoSync;
-
-extern int    gtkSubDumpMPSub;
-extern int    gtkSubDumpSrt;
-
-extern char * gtkEquChannel1;
-extern char * gtkEquChannel2;
-extern char * gtkEquChannel3;
-extern char * gtkEquChannel4;
-extern char * gtkEquChannel5;
-extern char * gtkEquChannel6;
-extern int    gtkLoadFullscreen;
-extern int    gtkShowVideoWindow;
-extern int    gtkEnablePlayBar;
-
-extern int    gui_save_pos;
-extern int    gui_main_pos_x;
-extern int    gui_main_pos_y;
-extern int    gui_sub_pos_x;
-extern int    gui_sub_pos_y;
+extern int gtkSubDumpMPSub;
+extern int gtkSubDumpSrt;
 
 #ifdef CONFIG_ASS
 typedef struct {
@@ -72,10 +77,21 @@ typedef struct {
     int top_margin;
     int bottom_margin;
 } gtkASS_t;
+
 extern gtkASS_t gtkASS;
 #endif
 
-int cfg_read( void );
-int cfg_write( void );
+extern int gtkEnablePlayBar;
+extern int gtkLoadFullscreen;
+extern int gtkShowVideoWindow;
+
+extern int gui_save_pos;
+extern int gui_main_pos_x;
+extern int gui_main_pos_y;
+extern int gui_sub_pos_x;
+extern int gui_sub_pos_y;
+
+int cfg_read(void);
+int cfg_write(void);
 
 #endif /* MPLAYER_GUI_CFG_H */
