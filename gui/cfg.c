601035b7aa07c3ef518fed641dd4ca4b6480b08f
ib <ib@b3059339-0415-0410-9bf9-f77b7e298cf2>
2011-06-06 14:10:09 +0000

    Cosmetic: Adjust indent.
    git-svn-id: svn://git.mplayerhq.hu/mplayer/trunk@33566 b3059339-0415-0410-9bf9-f77b7e298cf2

--- gui/cfg.c
+++ gui/cfg.c
@@ -97,55 +97,55 @@ int gui_sub_pos_x  = -3;
 int gui_sub_pos_y  = -3;
 
 static const m_option_t gui_opts[] = {
-    { "cache",                       &gtkCacheOn,              CONF_TYPE_FLAG,        0,          0,     1,       NULL },
-    { "cache_size",                  &gtkCacheSize,            CONF_TYPE_INT,         CONF_RANGE, 32,    1048576, NULL },
+    { "cache",                       &gtkCacheOn,              CONF_TYPE_FLAG,        0,           0,     1,       NULL },
+    { "cache_size",                  &gtkCacheSize,            CONF_TYPE_INT,         CONF_RANGE,  32,    1048576, NULL },
 
-    { "vf_pp",                       &gtkVfPP,                 CONF_TYPE_FLAG,        0,          0,     1,       NULL },
+    { "vf_pp",                       &gtkVfPP,                 CONF_TYPE_FLAG,        0,           0,     1,       NULL },
 
 #ifdef CONFIG_DXR3
-    { "vf_lavc",                     &gtkVfLAVC,               CONF_TYPE_FLAG,        0,          0,     1,       NULL },
-    { "vo_dxr3_device",              &gtkDXR3Device,           CONF_TYPE_STRING,      0,          0,     0,       NULL },
+    { "vf_lavc",                     &gtkVfLAVC,               CONF_TYPE_FLAG,        0,           0,     1,       NULL },
+    { "vo_dxr3_device",              &gtkDXR3Device,           CONF_TYPE_STRING,      0,           0,     0,       NULL },
 #endif
 
-    { "autosync",                    &gtkAutoSyncOn,           CONF_TYPE_FLAG,        0,          0,     1,       NULL },
-    { "autosync_size",               &gtkAutoSync,             CONF_TYPE_INT,         CONF_RANGE, 0,     10000,   NULL },
+    { "autosync",                    &gtkAutoSyncOn,           CONF_TYPE_FLAG,        0,           0,     1,       NULL },
+    { "autosync_size",               &gtkAutoSync,             CONF_TYPE_INT,         CONF_RANGE,  0,     10000,   NULL },
 
-    { "ao_volnorm",                  &gtkAONorm,               CONF_TYPE_FLAG,        0,          0,     1,       NULL },
-    { "ao_surround",                 &gtkAOSurround,           CONF_TYPE_FLAG,        0,          0,     1,       NULL },
-    { "ao_extra_stereo",             &gtkAOExtraStereo,        CONF_TYPE_FLAG,        0,          0,     1,       NULL },
-    { "ao_extra_stereo_coefficient", &gtkAOExtraStereoMul,     CONF_TYPE_FLOAT,       CONF_RANGE, -10.0, 10.0,    NULL },
+    { "ao_volnorm",                  &gtkAONorm,               CONF_TYPE_FLAG,        0,           0,     1,       NULL },
+    { "ao_surround",                 &gtkAOSurround,           CONF_TYPE_FLAG,        0,           0,     1,       NULL },
+    { "ao_extra_stereo",             &gtkAOExtraStereo,        CONF_TYPE_FLAG,        0,           0,     1,       NULL },
+    { "ao_extra_stereo_coefficient", &gtkAOExtraStereoMul,     CONF_TYPE_FLOAT,       CONF_RANGE,  -10.0, 10.0,    NULL },
 
 #ifdef CONFIG_ALSA
-    { "ao_alsa_mixer",               &gtkAOALSAMixer,          CONF_TYPE_STRING,      0,          0,     0,       NULL },
-    { "ao_alsa_mixer_channel",       &gtkAOALSAMixerChannel,   CONF_TYPE_STRING,      0,          0,     0,       NULL },
-    { "ao_alsa_device",              &gtkAOALSADevice,         CONF_TYPE_STRING,      0,          0,     0,       NULL },
+    { "ao_alsa_mixer",               &gtkAOALSAMixer,          CONF_TYPE_STRING,      0,           0,     0,       NULL },
+    { "ao_alsa_mixer_channel",       &gtkAOALSAMixerChannel,   CONF_TYPE_STRING,      0,           0,     0,       NULL },
+    { "ao_alsa_device",              &gtkAOALSADevice,         CONF_TYPE_STRING,      0,           0,     0,       NULL },
 #endif
 
 #ifdef CONFIG_OSS_AUDIO
-    { "ao_oss_mixer",                &gtkAOOSSMixer,           CONF_TYPE_STRING,      0,          0,     0,       NULL },
-    { "ao_oss_mixer_channel",        &gtkAOOSSMixerChannel,    CONF_TYPE_STRING,      0,          0,     0,       NULL },
-    { "ao_oss_device",               &gtkAOOSSDevice,          CONF_TYPE_STRING,      0,          0,     0,       NULL },
+    { "ao_oss_mixer",                &gtkAOOSSMixer,           CONF_TYPE_STRING,      0,           0,     0,       NULL },
+    { "ao_oss_mixer_channel",        &gtkAOOSSMixerChannel,    CONF_TYPE_STRING,      0,           0,     0,       NULL },
+    { "ao_oss_device",               &gtkAOOSSDevice,          CONF_TYPE_STRING,      0,           0,     0,       NULL },
 #endif
 
 #ifdef CONFIG_ESD
-    { "ao_esd_device",               &gtkAOESDDevice,          CONF_TYPE_STRING,      0,          0,     0,       NULL },
+    { "ao_esd_device",               &gtkAOESDDevice,          CONF_TYPE_STRING,      0,           0,     0,       NULL },
 #endif
 
 #ifdef CONFIG_SDL
-    { "ao_sdl_subdriver",            &gtkAOSDLDriver,          CONF_TYPE_STRING,      0,          0,     0,       NULL },
+    { "ao_sdl_subdriver",            &gtkAOSDLDriver,          CONF_TYPE_STRING,      0,           0,     0,       NULL },
 #endif
 
-    { "enable_audio_equ",            &gtkEnableAudioEqualizer, CONF_TYPE_FLAG,        0,          0,     1,       NULL },
+    { "enable_audio_equ",            &gtkEnableAudioEqualizer, CONF_TYPE_FLAG,        0,           0,     1,       NULL },
 
-    { "equ_channel_1",               &gtkEquChannel1,          CONF_TYPE_STRING,      0,          0,     0,       NULL },
-    { "equ_channel_2",               &gtkEquChannel2,          CONF_TYPE_STRING,      0,          0,     0,       NULL },
-    { "equ_channel_3",               &gtkEquChannel3,          CONF_TYPE_STRING,      0,          0,     0,       NULL },
-    { "equ_channel_4",               &gtkEquChannel4,          CONF_TYPE_STRING,      0,          0,     0,       NULL },
-    { "equ_channel_5",               &gtkEquChannel5,          CONF_TYPE_STRING,      0,          0,     0,       NULL },
-    { "equ_channel_6",               &gtkEquChannel6,          CONF_TYPE_STRING,      0,          0,     0,       NULL },
+    { "equ_channel_1",               &gtkEquChannel1,          CONF_TYPE_STRING,      0,           0,     0,       NULL },
+    { "equ_channel_2",               &gtkEquChannel2,          CONF_TYPE_STRING,      0,           0,     0,       NULL },
+    { "equ_channel_3",               &gtkEquChannel3,          CONF_TYPE_STRING,      0,           0,     0,       NULL },
+    { "equ_channel_4",               &gtkEquChannel4,          CONF_TYPE_STRING,      0,           0,     0,       NULL },
+    { "equ_channel_5",               &gtkEquChannel5,          CONF_TYPE_STRING,      0,           0,     0,       NULL },
+    { "equ_channel_6",               &gtkEquChannel6,          CONF_TYPE_STRING,      0,           0,     0,       NULL },
 
 #define audio_equ_row(i, j) \
-    { "equ_band_" # i # j,           &gtkEquChannels[i][j],    CONF_TYPE_FLOAT,       CONF_RANGE, -15.0, 15.0,    NULL }
+    { "equ_band_" # i # j,           &gtkEquChannels[i][j],    CONF_TYPE_FLOAT,       CONF_RANGE,  -15.0, 15.0,    NULL }
     audio_equ_row(0, 0), audio_equ_row(0, 1), audio_equ_row(0, 2), audio_equ_row(0, 3), audio_equ_row(0, 4), audio_equ_row(0, 5), audio_equ_row(0, 6), audio_equ_row(0, 7), audio_equ_row(0, 8), audio_equ_row(0, 9),
     audio_equ_row(1, 0), audio_equ_row(1, 1), audio_equ_row(1, 2), audio_equ_row(1, 3), audio_equ_row(1, 4), audio_equ_row(1, 5), audio_equ_row(1, 6), audio_equ_row(1, 7), audio_equ_row(1, 8), audio_equ_row(1, 9),
     audio_equ_row(2, 0), audio_equ_row(2, 1), audio_equ_row(2, 2), audio_equ_row(2, 3), audio_equ_row(2, 4), audio_equ_row(2, 5), audio_equ_row(2, 6), audio_equ_row(2, 7), audio_equ_row(2, 8), audio_equ_row(2, 9),
@@ -154,75 +154,75 @@ static const m_option_t gui_opts[] = {
     audio_equ_row(5, 0), audio_equ_row(5, 1), audio_equ_row(5, 2), audio_equ_row(5, 3), audio_equ_row(5, 4), audio_equ_row(5, 5), audio_equ_row(5, 6), audio_equ_row(5, 7), audio_equ_row(5, 8), audio_equ_row(5, 9),
 #undef audio_equ_row
 
-    { "playbar",                     &gtkEnablePlayBar,        CONF_TYPE_FLAG,        0,          0,     1,       NULL },
-    { "load_fullscreen",             &gtkLoadFullscreen,       CONF_TYPE_FLAG,        0,          0,     1,       NULL },
-    { "show_videowin",               &gtkShowVideoWindow,      CONF_TYPE_FLAG,        0,          0,     1,       NULL },
+    { "playbar",                     &gtkEnablePlayBar,        CONF_TYPE_FLAG,        0,           0,     1,       NULL },
+    { "load_fullscreen",             &gtkLoadFullscreen,       CONF_TYPE_FLAG,        0,           0,     1,       NULL },
+    { "show_videowin",               &gtkShowVideoWindow,      CONF_TYPE_FLAG,        0,           0,     1,       NULL },
 
-    { "gui_save_pos",                &gui_save_pos,            CONF_TYPE_FLAG,        0,          0,     1,       NULL },
-    { "gui_main_pos_x",              &gui_main_pos_x,          CONF_TYPE_INT,         0,          0,     0,       NULL },
-    { "gui_main_pos_y",              &gui_main_pos_y,          CONF_TYPE_INT,         0,          0,     0,       NULL },
-    { "gui_video_out_pos_x",         &gui_sub_pos_x,           CONF_TYPE_INT,         0,          0,     0,       NULL },
-    { "gui_video_out_pos_y",         &gui_sub_pos_y,           CONF_TYPE_INT,         0,          0,     0,       NULL },
+    { "gui_save_pos",                &gui_save_pos,            CONF_TYPE_FLAG,        0,           0,     1,       NULL },
+    { "gui_main_pos_x",              &gui_main_pos_x,          CONF_TYPE_INT,         0,           0,     0,       NULL },
+    { "gui_main_pos_y",              &gui_main_pos_y,          CONF_TYPE_INT,         0,           0,     0,       NULL },
+    { "gui_video_out_pos_x",         &gui_sub_pos_x,           CONF_TYPE_INT,         0,           0,     0,       NULL },
+    { "gui_video_out_pos_y",         &gui_sub_pos_y,           CONF_TYPE_INT,         0,           0,     0,       NULL },
 
     { "idle",                        &player_idle_mode,        CONF_TYPE_FLAG,        CONF_GLOBAL, 0,     1,       NULL },
 
     // NOTE TO MYSELF: Do we really need all/any non-gtkOptions, i.e. override mplayer options?
 
-    { "gui_skin",                    &skinName,                CONF_TYPE_STRING,      0,          0,     0,       NULL },
+    { "gui_skin",                    &skinName,                CONF_TYPE_STRING,      0,           0,     0,       NULL },
 
-    { "stopxscreensaver",            &stop_xscreensaver,       CONF_TYPE_FLAG,        0,          0,     1,       NULL },
+    { "stopxscreensaver",            &stop_xscreensaver,       CONF_TYPE_FLAG,        0,           0,     1,       NULL },
 
-    { "cdrom_device",                &cdrom_device,            CONF_TYPE_STRING,      0,          0,     0,       NULL },
-    { "dvd_device",                  &dvd_device,              CONF_TYPE_STRING,      0,          0,     0,       NULL },
+    { "cdrom_device",                &cdrom_device,            CONF_TYPE_STRING,      0,           0,     0,       NULL },
+    { "dvd_device",                  &dvd_device,              CONF_TYPE_STRING,      0,           0,     0,       NULL },
 
-    { "osd_level",                   &osd_level,               CONF_TYPE_INT,         CONF_RANGE, 0,     3,       NULL },
+    { "osd_level",                   &osd_level,               CONF_TYPE_INT,         CONF_RANGE,  0,     3,       NULL },
 
-    { "vo_driver",                   &video_driver_list,       CONF_TYPE_STRING_LIST, 0,          0,     0,       NULL },
-    { "v_vfm",                       &video_fm_list,           CONF_TYPE_STRING_LIST, 0,          0,     0,       NULL },
+    { "vo_driver",                   &video_driver_list,       CONF_TYPE_STRING_LIST, 0,           0,     0,       NULL },
+    { "v_vfm",                       &video_fm_list,           CONF_TYPE_STRING_LIST, 0,           0,     0,       NULL },
 
-    { "vf_autoq",                    &auto_quality,            CONF_TYPE_INT,         CONF_RANGE, 0,     100,     NULL },
-    { "vo_direct_render",            &vo_directrendering,      CONF_TYPE_FLAG,        0,          0,     1,       NULL },
-    { "vo_doublebuffering",          &vo_doublebuffering,      CONF_TYPE_FLAG,        0,          0,     1,       NULL },
-    { "vo_panscan",                  &vo_panscan,              CONF_TYPE_FLOAT,       CONF_RANGE, 0.0,   1.0,     NULL },
+    { "vf_autoq",                    &auto_quality,            CONF_TYPE_INT,         CONF_RANGE,  0,     100,     NULL },
+    { "vo_direct_render",            &vo_directrendering,      CONF_TYPE_FLAG,        0,           0,     1,       NULL },
+    { "vo_doublebuffering",          &vo_doublebuffering,      CONF_TYPE_FLAG,        0,           0,     1,       NULL },
+    { "vo_panscan",                  &vo_panscan,              CONF_TYPE_FLOAT,       CONF_RANGE,  0.0,   1.0,     NULL },
 
-    { "v_flip",                      &flip,                    CONF_TYPE_INT,         CONF_RANGE, -1,    1,       NULL },
-    { "v_framedrop",                 &frame_dropping,          CONF_TYPE_INT,         CONF_RANGE, 0,     2,       NULL },
-    { "v_idx",                       &index_mode,              CONF_TYPE_INT,         CONF_RANGE, -1,    2,       NULL },
-    { "v_ni",                        &force_ni,                CONF_TYPE_FLAG,        0,          0,     1,       NULL },
+    { "v_flip",                      &flip,                    CONF_TYPE_INT,         CONF_RANGE,  -1,    1,       NULL },
+    { "v_framedrop",                 &frame_dropping,          CONF_TYPE_INT,         CONF_RANGE,  0,     2,       NULL },
+    { "v_idx",                       &index_mode,              CONF_TYPE_INT,         CONF_RANGE,  -1,    2,       NULL },
+    { "v_ni",                        &force_ni,                CONF_TYPE_FLAG,        0,           0,     1,       NULL },
 
-    { "ao_driver",                   &audio_driver_list,       CONF_TYPE_STRING_LIST, 0,          0,     0,       NULL },
-    { "a_afm",                       &audio_fm_list,           CONF_TYPE_STRING_LIST, 0,          0,     0,       NULL },
+    { "ao_driver",                   &audio_driver_list,       CONF_TYPE_STRING_LIST, 0,           0,     0,       NULL },
+    { "a_afm",                       &audio_fm_list,           CONF_TYPE_STRING_LIST, 0,           0,     0,       NULL },
 
-    { "softvol",                     &soft_vol,                CONF_TYPE_FLAG,        0,          0,     1,       NULL },
+    { "softvol",                     &soft_vol,                CONF_TYPE_FLAG,        0,           0,     1,       NULL },
 
-    { "sub_auto_load",               &sub_auto,                CONF_TYPE_FLAG,        0,          0,     1,       NULL },
+    { "sub_auto_load",               &sub_auto,                CONF_TYPE_FLAG,        0,           0,     1,       NULL },
 #ifdef CONFIG_ICONV
-    { "sub_cp",                      &sub_cp,                  CONF_TYPE_STRING,      0,          0,     0,       NULL },
+    { "sub_cp",                      &sub_cp,                  CONF_TYPE_STRING,      0,           0,     0,       NULL },
 #endif
-    { "sub_overlap",                 &suboverlap_enabled,      CONF_TYPE_FLAG,        0,          0,     0,       NULL },
-    { "sub_pos",                     &sub_pos,                 CONF_TYPE_INT,         CONF_RANGE, 0,     200,     NULL },
-    { "sub_unicode",                 &sub_unicode,             CONF_TYPE_FLAG,        0,          0,     1,       NULL },
+    { "sub_overlap",                 &suboverlap_enabled,      CONF_TYPE_FLAG,        0,           0,     0,       NULL },
+    { "sub_pos",                     &sub_pos,                 CONF_TYPE_INT,         CONF_RANGE,  0,     200,     NULL },
+    { "sub_unicode",                 &sub_unicode,             CONF_TYPE_FLAG,        0,           0,     1,       NULL },
 
-    { "font_factor",                 &font_factor,             CONF_TYPE_FLOAT,       CONF_RANGE, 0.0,   10.0,    NULL },
-    { "font_name",                   &font_name,               CONF_TYPE_STRING,      0,          0,     0,       NULL },
+    { "font_factor",                 &font_factor,             CONF_TYPE_FLOAT,       CONF_RANGE,  0.0,   10.0,    NULL },
+    { "font_name",                   &font_name,               CONF_TYPE_STRING,      0,           0,     0,       NULL },
 
 #ifdef CONFIG_FREETYPE
-    { "font_encoding",               &subtitle_font_encoding,  CONF_TYPE_STRING,      0,          0,     0,       NULL },
-    { "font_text_scale",             &text_font_scale_factor,  CONF_TYPE_FLOAT,       CONF_RANGE, 0.0,   100.0,   NULL },
-    { "font_osd_scale",              &osd_font_scale_factor,   CONF_TYPE_FLOAT,       CONF_RANGE, 0.0,   100.0,   NULL },
-    { "font_blur",                   &subtitle_font_radius,    CONF_TYPE_FLOAT,       CONF_RANGE, 0.0,   8.0,     NULL },
-    { "font_outline",                &subtitle_font_thickness, CONF_TYPE_FLOAT,       CONF_RANGE, 0.0,   8.0,     NULL },
-    { "font_autoscale",              &subtitle_autoscale,      CONF_TYPE_INT,         CONF_RANGE, 0,     3,       NULL },
+    { "font_encoding",               &subtitle_font_encoding,  CONF_TYPE_STRING,      0,           0,     0,       NULL },
+    { "font_text_scale",             &text_font_scale_factor,  CONF_TYPE_FLOAT,       CONF_RANGE,  0.0,   100.0,   NULL },
+    { "font_osd_scale",              &osd_font_scale_factor,   CONF_TYPE_FLOAT,       CONF_RANGE,  0.0,   100.0,   NULL },
+    { "font_blur",                   &subtitle_font_radius,    CONF_TYPE_FLOAT,       CONF_RANGE,  0.0,   8.0,     NULL },
+    { "font_outline",                &subtitle_font_thickness, CONF_TYPE_FLOAT,       CONF_RANGE,  0.0,   8.0,     NULL },
+    { "font_autoscale",              &subtitle_autoscale,      CONF_TYPE_INT,         CONF_RANGE,  0,     3,       NULL },
 #endif
 
 #ifdef CONFIG_ASS
-    { "ass_enabled",                 &ass_enabled,             CONF_TYPE_FLAG,        0,          0,     1,       NULL },
-    { "ass_use_margins",             &ass_use_margins,         CONF_TYPE_FLAG,        0,          0,     1,       NULL },
-    { "ass_top_margin",              &ass_top_margin,          CONF_TYPE_INT,         CONF_RANGE, 0,     512,     NULL },
-    { "ass_bottom_margin",           &ass_bottom_margin,       CONF_TYPE_INT,         CONF_RANGE, 0,     512,     NULL },
+    { "ass_enabled",                 &ass_enabled,             CONF_TYPE_FLAG,        0,           0,     1,       NULL },
+    { "ass_use_margins",             &ass_use_margins,         CONF_TYPE_FLAG,        0,           0,     1,       NULL },
+    { "ass_top_margin",              &ass_top_margin,          CONF_TYPE_INT,         CONF_RANGE,  0,     512,     NULL },
+    { "ass_bottom_margin",           &ass_bottom_margin,       CONF_TYPE_INT,         CONF_RANGE,  0,     512,     NULL },
 #endif
 
-    { NULL,                          NULL,                     0,                     0,          0,     0,       NULL }
+    { NULL,                          NULL,                     0,                     0,           0,     0,       NULL }
 };
 
 static char *gfgets(char *str, int size, FILE *f)
