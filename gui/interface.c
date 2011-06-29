d88a2acc657263fba19f55f58e225dc9e875d059
ib <ib@b3059339-0415-0410-9bf9-f77b7e298cf2>
2011-06-29 10:25:02 +0000

    Don't stop playing after a DXR3 demuxer type error.
    Instead, continue in the playlist.
    
    
    git-svn-id: svn://git.mplayerhq.hu/mplayer/trunk@33771 b3059339-0415-0410-9bf9-f77b7e298cf2

--- gui/interface.c
+++ gui/interface.c
@@ -739,7 +739,6 @@ int guiGetEvent(int type, void *arg)
 #ifdef CONFIG_DXR3
         if (video_driver_list && !gstrcmp(video_driver_list[0], "dxr3") && (((demuxer_t *)mpctx_get_demuxer(guiInfo.mpcontext))->file_format != DEMUXER_TYPE_MPEG_PS) && !gtkVfLAVC) {
             gtkMessageBox(GTK_MB_FATAL, MSGTR_NEEDLAVC);
-            guiInfo.Playing = 0;
             return False;
         }
 #endif
