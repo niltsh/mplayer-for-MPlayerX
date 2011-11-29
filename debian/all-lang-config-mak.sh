1c65370d80a96bd757e313452f1d1b10c51f015b
diego <diego@b3059339-0415-0410-9bf9-f77b7e298cf2>
2011-11-29 05:05:16 +0000

    debian: adjust documentation build process, which now works non-recursively
    git-svn-id: svn://git.mplayerhq.hu/mplayer/trunk@34365 b3059339-0415-0410-9bf9-f77b7e298cf2

--- debian/all-lang-config-mak.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# hack to avoid having to configure mplayer just for building the
-# xml documentation
-
-doc_lang_all=`echo DOCS/xml/??/ DOCS/xml/??_??/ | sed -e "s:DOCS/xml/\(..\)/:\1:g" -e "s:DOCS/xml/\(.._..\)/:\1:g"`
-
-echo "DOC_LANG_ALL = ${doc_lang_all}"
-echo "DOC_LANGS = ${doc_lang_all}"
