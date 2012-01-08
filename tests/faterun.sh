5933588952673ec73627ce9e05042fad0c68a6f4
reimar <reimar@b3059339-0415-0410-9bf9-f77b7e298cf2>
2012-01-08 09:28:51 +0000

    Clean up faterun.sh.
    Better variable names, less duplicated code, some whitespace, variable quoting...
    
    
    git-svn-id: svn://git.mplayerhq.hu/mplayer/trunk@34528 b3059339-0415-0410-9bf9-f77b7e298cf2

--- tests/faterun.sh
+++ tests/faterun.sh
@@ -1,15 +1,25 @@
 #!/bin/sh
-i=$1
-echo "running $i"
-mkdir -p tests/res/$(dirname $i)
-touch tests/res/$i.md5
-./mplayer -noconfig all -lavdopts threads=4:bitexact -really-quiet -noconsolecontrols -nosound -benchmark -vo md5sum:outfile=tests/res/$i.md5 $FATE_SAMPLES/$i
-ref_file=tests/ref/$i.md5
-if ! [ -e $ref_file ] ; then
+if [ -z "$FATE_SAMPLES" ] ; then
+  echo "FATE_SAMPLES is not set!"
+  exit 1
+fi
+
+sample="$1"
+md5out="tests/res/$sample.md5"
+ref_file="tests/ref/$sample.md5"
+echo "testing $sample"
+
+# create necessary files and run
+mkdir -p $(dirname "$md5out")
+touch "$md5out"
+./mplayer -noconfig all -lavdopts threads=4:bitexact -really-quiet -noconsolecontrols -nosound -benchmark -vo md5sum:outfile="$md5out" "$FATE_SAMPLES/$sample"
+
+# check result
+if ! [ -e "$ref_file" ] ; then
   touch tests/ref/empty.md5
   ref_file=tests/ref/empty.md5
 fi
-if ! diff -uw $ref_file tests/res/$i.md5 ; then
-  mv tests/res/$i.md5 tests/res/$i.md5.bad
+if ! diff -uw "$ref_file" "$md5out" ; then
+  mv "$md5out" "$md5out.bad"
   exit 1
 fi
