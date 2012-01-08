acb12ac653fa18589a397f51324a5eed7b1885c9
reimar <reimar@b3059339-0415-0410-9bf9-f77b7e298cf2>
2012-01-08 10:44:00 +0000

    Add a script to help update the references.
    git-svn-id: svn://git.mplayerhq.hu/mplayer/trunk@34532 b3059339-0415-0410-9bf9-f77b7e298cf2

---
+++ tests/refupdate.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# updates all changed/new results in ref/
+find res -name '*.bad' | while read bad_res ; do
+  ref_file="ref/${bad_res#res/}"
+  ref_file="${ref_file%.bad}"
+  mkdir -p $(dirname "ref_file")
+  cp "$bad_res" "$ref_file"
+done
