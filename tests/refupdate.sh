48cd1ea742e68f7e72b2495700d6236490f283f9
reimar <reimar@b3059339-0415-0410-9bf9-f77b7e298cf2>
2012-01-08 15:38:22 +0000

    Fix directory creation in refupdate script.
    git-svn-id: svn://git.mplayerhq.hu/mplayer/trunk@34536 b3059339-0415-0410-9bf9-f77b7e298cf2

--- tests/refupdate.sh
+++ tests/refupdate.sh
@@ -3,6 +3,6 @@
 find res -name '*.bad' | while read bad_res ; do
   ref_file="ref/${bad_res#res/}"
   ref_file="${ref_file%.bad}"
-  mkdir -p $(dirname "ref_file")
+  mkdir -p "$(dirname "$ref_file")"
   cp "$bad_res" "$ref_file"
 done
