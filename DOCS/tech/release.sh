d319eab2f7f2f1c5fcbb57cc63b163d2dd16aec6
reimar <reimar@b3059339-0415-0410-9bf9-f77b7e298cf2>
2012-06-02 18:09:58 +0000

    Another minor release script update.
    git-svn-id: svn://git.mplayerhq.hu/mplayer/trunk@34991 b3059339-0415-0410-9bf9-f77b7e298cf2

--- DOCS/tech/release.sh
+++ DOCS/tech/release.sh
@@ -5,9 +5,11 @@ dst=MPlayer-$ver
 svnurl=svn://svn.mplayerhq.hu/mplayer/trunk
 #svnurl=svn://svn.mplayerhq.hu/mplayer/branches/$ver
 
-rm -rf $dst/ $dst-DOCS/
+rm -rf $dst/ $dst-DOCS/ $dst.tar*
 
 svn export $svnurl $dst/
+# branches should contain a VERSION file, but
+# "previews" generated from trunk need it.
 if ! test -e $dst/VERSION ; then
   echo $ver > $dst/VERSION
 fi
@@ -26,6 +28,15 @@ rm -rf $dst-DOCS/
 git clone --depth 1 git://git.videolan.org/ffmpeg.git $dst/ffmpeg
 rm -rf $dst/.git* $dst/ffmpeg/.git*
 
+# create .tar.gz and .tar.xz files
 tar --owner=0 --group=0 -cf $dst.tar $dst/
 xz -k $dst.tar
 gzip -9 $dst.tar
+
+# generate checksums/signatures
+md5sum $dst.tar.xz > $dst.tar.xz.md5
+sha1sum $dst.tar.xz > $dst.tar.xz.sha1
+md5sum $dst.tar.gz > $dst.tar.gz.md5
+sha1sum $dst.tar.gz > $dst.tar.gz.sha1
+gpg -a --detach-sign $dst.tar.xz
+gpg -a --detach-sign $dst.tar.gz
