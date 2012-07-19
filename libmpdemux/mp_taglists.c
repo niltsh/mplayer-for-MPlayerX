51d3494ca3e80fda9f0ee8537452e288f1be4a7b
cehoyos <cehoyos@b3059339-0415-0410-9bf9-f77b7e298cf2>
2012-07-19 06:23:54 +0000

    Support FFmpeg APC ADPCM IMA audio decoder
    Patch by Paul B Mahol, onemda gmail
    
    
    git-svn-id: svn://git.mplayerhq.hu/mplayer/trunk@35035 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libmpdemux/mp_taglists.c
+++ libmpdemux/mp_taglists.c
@@ -29,6 +29,7 @@
 static const struct AVCodecTag mp_wav_tags[] = {
     { CODEC_ID_ADPCM_4XM,         MKTAG('4', 'X', 'M', 'A')},
     { CODEC_ID_ADPCM_ADX,         MKTAG('S', 'a', 'd', 'x')},
+    { CODEC_ID_ADPCM_IMA_APC,     MKTAG('A', 'P', 'C', 'A')},
     { CODEC_ID_ADPCM_EA,          MKTAG('A', 'D', 'E', 'A')},
     { CODEC_ID_ADPCM_EA_R1,       MKTAG('E', 'A', 'R', '1')},
     { CODEC_ID_ADPCM_EA_R2,       MKTAG('E', 'A', 'R', '2')},
