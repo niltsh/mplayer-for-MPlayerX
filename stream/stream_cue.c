d6fc411af7fe0ae793fc7152c0cce971dc89790b
reimar <reimar@b3059339-0415-0410-9bf9-f77b7e298cf2>
2012-11-10 14:21:16 +0000

    Do not call fstat if open failed.
    git-svn-id: svn://git.mplayerhq.hu/mplayer/trunk@35405 b3059339-0415-0410-9bf9-f77b7e298cf2

--- stream/stream_cue.c
+++ stream/stream_cue.c
@@ -249,7 +249,7 @@ static int cue_find_bin (const char *firstline) {
       break;
     }
     fd_bin = open(cur_name, O_RDONLY);
-    if (fstat(fd_bin, &filestat) == -1 || !S_ISREG(filestat.st_mode)) {
+    if (fd_bin != -1 && (fstat(fd_bin, &filestat) == -1 || !S_ISREG(filestat.st_mode))) {
         close(fd_bin);
         fd_bin = -1;
     }
