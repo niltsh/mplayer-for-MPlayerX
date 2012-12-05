9088f67f4acf6600287d2dc63062bd39bc3ad9c6
cehoyos <cehoyos@b3059339-0415-0410-9bf9-f77b7e298cf2>
2012-12-05 08:17:37 +0000

    Recognize audio/ogg as an OGG mimetype.
    Patch by Ben Boeckel, mathstuf gmail
    
    
    git-svn-id: svn://git.mplayerhq.hu/mplayer/trunk@35602 b3059339-0415-0410-9bf9-f77b7e298cf2

--- stream/network.c
+++ stream/network.c
@@ -104,6 +104,7 @@ const mime_struct_t mime_type_table[] = {
 	// OGG Streaming
 	{ "application/ogg", DEMUXER_TYPE_OGG },
 	{ "application/x-ogg", DEMUXER_TYPE_OGG },
+	{ "audio/ogg", DEMUXER_TYPE_OGG },
 	// NullSoft Streaming Video
 	{ "video/nsv", DEMUXER_TYPE_NSV},
 	{ "misc/ultravox", DEMUXER_TYPE_NSV},
