b133a4897b7ad340c087dd1f16719542973e0e77
ib <ib@b3059339-0415-0410-9bf9-f77b7e298cf2>
2012-12-10 01:43:38 +0000

    Change parameter from NULL to 0.
    This is for optical reasons and to reflect that nothing is passed.
    
    
    git-svn-id: svn://git.mplayerhq.hu/mplayer/trunk@35642 b3059339-0415-0410-9bf9-f77b7e298cf2

--- gui/dialog/playlist.c
+++ gui/dialog/playlist.c
@@ -236,7 +236,7 @@ static void plButtonReleased( GtkButton * button,gpointer user_data )
 	    guiInfo.PlaylistNext = (curr.name ? False : True);
 	    guiInfo.Track = 1;
 	   }
-	  guiInfo.Tracks = (int) listMgr( PLAYLIST_ITEM_GET_POS,NULL );
+	  guiInfo.Tracks = (int) listMgr( PLAYLIST_ITEM_GET_POS,0 );
 	 }
 	else if (isPlaylistStreamtype && !guiInfo.Playing) uiUnsetFile();
 	free(curr.path);
