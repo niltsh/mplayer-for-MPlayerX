00bfd553322e91041e18bf45b0c95241e76413fa
ib <ib@b3059339-0415-0410-9bf9-f77b7e298cf2>
2013-01-18 17:53:44 +0000

    Remove unnecessary #includes.
    git-svn-id: svn://git.mplayerhq.hu/mplayer/trunk@35783 b3059339-0415-0410-9bf9-f77b7e298cf2

--- gui/wm/ws.h
+++ gui/wm/ws.h
@@ -23,12 +23,11 @@
 #define MPLAYER_GUI_WS_H
 
 #include "gui/dialog/dialog.h"
+
 #include "config.h"
 
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
-#include <X11/keysym.h>
-#include <X11/Xatom.h>
 #ifdef HAVE_SHM
 #include <X11/extensions/XShm.h>
 #endif
