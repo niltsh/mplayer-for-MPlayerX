10f8eb876441bbc3c67fcc8cea1cc73ed4a220b3
ib <ib@b3059339-0415-0410-9bf9-f77b7e298cf2>
2013-03-27 18:58:32 +0000

    Change variable definitions to const.
    Additionally, rename fsThatDir fsThisDir.
    
    
    git-svn-id: svn://git.mplayerhq.hu/mplayer/trunk@36063 b3059339-0415-0410-9bf9-f77b7e298cf2

--- gui/dialog/fileselect.c
+++ gui/dialog/fileselect.c
@@ -56,8 +56,8 @@
 char * get_current_dir_name( void );
 #endif
 
-char          * fsSelectedFile = NULL;
-unsigned char * fsThatDir = ".";
+const char    * fsSelectedFile = NULL;
+const char    * fsThisDir = ".";
 const gchar   * fsFilter = "*";
 
 int             fsType    = 0;
@@ -376,7 +376,7 @@ static void fs_Up_released( GtkButton * button, gpointer user_data )
  utf8dir = get_current_dir_name_utf8();
  gtk_entry_set_text( GTK_ENTRY( fsPathCombo ),utf8dir );
  g_free(utf8dir);
- fsSelectedFile=fsThatDir;
+ fsSelectedFile=fsThisDir;
  return;
 }
 
@@ -396,7 +396,7 @@ static void fs_Ok_released( GtkButton * button, gpointer user_data )
    gtk_entry_set_text( GTK_ENTRY( fsPathCombo ),utf8dir );
    g_free(utf8dir);
    gtk_widget_grab_focus( fsFNameList );
-   fsSelectedFile=fsThatDir;
+   fsSelectedFile=fsThisDir;
    return;
   }
 
