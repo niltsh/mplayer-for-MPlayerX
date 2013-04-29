8789f113d8670b8310b27485652ff59e58dc71d3
ib <ib@b3059339-0415-0410-9bf9-f77b7e298cf2>
2013-04-29 11:47:47 +0000

    Cosmetic: Adjust indent.
    git-svn-id: svn://git.mplayerhq.hu/mplayer/trunk@36181 b3059339-0415-0410-9bf9-f77b7e298cf2

--- gui/dialog/url.c
+++ gui/dialog/url.c
@@ -61,15 +61,15 @@ static void button_clicked(GtkButton *button, gpointer user_data)
             }
 
             if (str) {
-            item      = calloc(1, sizeof(urlItem));
-            item->url = str;
-            listMgr(URLLIST_ITEM_ADD, item);
+                item      = calloc(1, sizeof(urlItem));
+                item->url = str;
+                listMgr(URLLIST_ITEM_ADD, item);
 
-            uiSetFile(NULL, str, STREAMTYPE_STREAM);
-            listMgr(PLAYLIST_DELETE, 0);
-            add_to_gui_playlist(str, PLAYLIST_ITEM_APPEND);
-            guiInfo.NewPlay = GUI_FILE_NEW;
-            uiEvent(evPlay, 0);
+                uiSetFile(NULL, str, STREAMTYPE_STREAM);
+                listMgr(PLAYLIST_DELETE, 0);
+                add_to_gui_playlist(str, PLAYLIST_ITEM_APPEND);
+                guiInfo.NewPlay = GUI_FILE_NEW;
+                uiEvent(evPlay, 0);
             }
         }
     }
