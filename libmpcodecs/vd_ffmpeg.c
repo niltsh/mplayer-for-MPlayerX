f7baea25de4531e7be109f0771223df2fbf3b3fe
reimar <reimar@b3059339-0415-0410-9bf9-f77b7e298cf2>
2013-05-05 07:27:32 +0000

    Less strict check for width/height.
    Due to cropping AVFrame dimensions can be larger than the display size.
    
    git-svn-id: svn://git.mplayerhq.hu/mplayer/trunk@36236 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libmpcodecs/vd_ffmpeg.c
+++ libmpcodecs/vd_ffmpeg.c
@@ -940,8 +940,8 @@ static mp_image_t *decode(sh_video_t *sh, void *data, int len, int flags){
         return NULL;
     }
 
-    if (mpi->w != pic->width || mpi->h != pic->height ||
-        pic->width != avctx->width || pic->height != avctx->height) {
+    if (mpi->w != avctx->width || mpi->h != avctx->height ||
+        pic->width < mpi->w || pic->height < mpi->h) {
         mp_msg(MSGT_DECVIDEO, MSGL_ERR, "Dropping frame with size not matching configured size (%ix%i vs %ix%i vs %ix%i)\n",
                mpi->w, mpi->h, pic->width, pic->height, avctx->width, avctx->height);
         return NULL;
