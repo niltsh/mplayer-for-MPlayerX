829f298e8c4727f32c65d138434e412ef8f30116
reimar <reimar@b3059339-0415-0410-9bf9-f77b7e298cf2>
2013-05-09 14:06:15 +0000

    Remove XSelectInput and XSync call that do not seem to serve any purpose.
    They existed since the first version of vo_gl and simply
    ended up being carried on.
    This could cause events to be lost.
    Part of a patch by Jens Stimpfle [debian jstimpfle de]
    
    git-svn-id: svn://git.mplayerhq.hu/mplayer/trunk@36260 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libvo/x11_common.c
+++ libvo/x11_common.c
@@ -1181,8 +1181,6 @@ void vo_x11_create_vo_window(XVisualInfo *vis, int x, int y,
       XNextEvent(mDisplay, &xev);
     } while (xev.type != MapNotify || xev.xmap.event != vo_window);
     vo_x11_clearwindow(mDisplay, vo_window);
-    XSelectInput(mDisplay, vo_window, NoEventMask);
-    XSync(mDisplay, False);
     vo_x11_selectinput_witherr(mDisplay, vo_window,
           StructureNotifyMask | KeyPressMask | KeyReleaseMask | PointerMotionMask |
           ButtonPressMask | ButtonReleaseMask | ExposureMask);
