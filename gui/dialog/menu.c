7367867419e65c4505212e02cf9d29fdf1dc3873
ib <ib@b3059339-0415-0410-9bf9-f77b7e298cf2>
2013-08-01 21:20:14 +0000

    Adjust detection of original aspect ratio after r36391.
    git-svn-id: svn://git.mplayerhq.hu/mplayer/trunk@36392 b3059339-0415-0410-9bf9-f77b7e298cf2

--- gui/dialog/menu.c
+++ gui/dialog/menu.c
@@ -584,7 +584,7 @@ GtkWidget * CreatePopUpMenu( void )
    {
     int a11 = False, a169 = False, a43 = False, a235 = False;
 
-    if (movie_aspect == -1 || FFABS(movie_aspect - (float) guiInfo.sh_video->disp_w / guiInfo.sh_video->disp_h) <= 0.01f) a11 = True;
+    if (FFABS(guiInfo.sh_video->aspect - guiInfo.sh_video->original_aspect) <= 0.01f) a11 = True;
     else
      {
        a169 = (FFABS(movie_aspect - 16.0f / 9.0f) <= 0.01f);
