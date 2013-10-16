d5171bb5268154535be87583c950d5e880fa047f
reimar <reimar@b3059339-0415-0410-9bf9-f77b7e298cf2>
2013-10-16 17:47:16 +0000

    vd_ffmpeg: catch invalid aspect from FFmpeg.
    This avoids endless reinitialization with buggy FFmpeg
    codecs like HEVC that set aspect to be n/0, which compares
    unequal to itself.
    
    git-svn-id: svn://git.mplayerhq.hu/mplayer/trunk@36469 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libmpcodecs/vd_ffmpeg.c
+++ libmpcodecs/vd_ffmpeg.c
@@ -546,6 +546,10 @@ static void draw_slice(struct AVCodecContext *s,
 static void update_configuration(sh_video_t *sh, enum AVPixelFormat pix_fmt) {
     vd_ffmpeg_ctx *ctx = sh->context;
     AVCodecContext *avctx = ctx->avctx;
+    if (!avctx->sample_aspect_ratio.den) {
+        mp_msg(MSGT_DECVIDEO, MSGL_WARN, "FFmpeg bug: Invalid aspect\n");
+        avctx->sample_aspect_ratio.den = 1;
+    }
      // it is possible another vo buffers to be used after vo config()
      // lavc reset its buffers on width/heigh change but not on aspect change!!!
     if (av_cmp_q(avctx->sample_aspect_ratio, ctx->last_sample_aspect_ratio) ||
